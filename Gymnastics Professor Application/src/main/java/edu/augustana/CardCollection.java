5cbf6d33b84d027cb738a4cf968689047d447d91
Joshua-fosu <joshuafosu119@gmail.com>
2023-10-03 23:49:44 -0400

    Implemented the Card Collection list

---
+++ Gymnastics Professor Application/src/main/java/edu/augustana/CardCollection.java
@@ -0,0 +1,10 @@
+package edu.augustana;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CardCollection {
+
+    public static List<Card> cardCollection = new ArrayList<Card>();
+
+}
