4f74132dc2834330bca1bd8098417cb44d470f5e
Joshua-fosu <joshuafosu119@gmail.com>
2023-10-11 00:52:51 -0400

    Cards are displayed on main screen

---
+++ Gymnastics Professor Application/src/main/java/edu/augustana/CardView.java
@@ -0,0 +1,43 @@
+package edu.augustana;
+
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
+
+import java.io.File;
+
+public class CardView {
+    private Card mCard;
+
+    public CardView(Card card) {
+        this.mCard = card;
+    }
+
+    public VBox makeCardView() {
+        VBox vBox = new VBox();
+        ImageView imageView = new ImageView();
+        vBox.setMinWidth(500);
+        vBox.setMinHeight(500);
+
+        try {
+
+            // Load the image from the resource path
+
+            Image image = new Image(GymnasticsProfessorApp.class.getResource("DEMO1Pack/" + mCard.getCardImage()).toString());
+            // Create an ImageView and set the image
+            imageView = new ImageView(image);
+            imageView.setFitHeight(500);
+            imageView.setFitWidth(500);
+
+            // Add the ImageView to the VBox
+            vBox.getChildren().add(imageView);
+        } catch (Exception e) {
+            System.out.print(mCard.getCardImage());
+        }
+
+
+        return vBox;
+    }
+
+}
