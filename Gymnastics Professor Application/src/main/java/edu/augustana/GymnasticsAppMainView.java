bb2b56c75310933acb87e5ac032151359c53d55c
aslaoui03 <144163544+aslaoui03@users.noreply.github.com>
2023-10-30 12:22:23 -0500

    Revert "merge conflicts"
    This reverts commit 35be9af494710c0b092f45ed8d7b4007632c1011, reversing
    changes made to 3fd14f2c0236ab518554e4cd8a67630af6625d19.

--- Gymnastics Professor Application/src/main/java/edu/augustana/GymnasticsAppMainView.java
+++ Gymnastics Professor Application/src/main/java/edu/augustana/GymnasticsAppMainView.java
@@ -206,19 +206,11 @@ public class GymnasticsAppMainView {
         }
     };
 
-
-    @FXML
-    void addImage(MouseEvent event) {
-        lessonPlanImage.setVisible(true);
-    }
-
-
     @FXML
     void clearImage(MouseEvent event){
         lessonPlanImage.setVisible(false);
     }
-
-     public void addToLessonPlan(Card mCard) {
+    public void addToLessonPlan(Card mCard) {
 
         lessonPlan.add(mCard);
         lessonPlanCardView.getItems().add(mCard);
@@ -253,5 +245,7 @@ public class GymnasticsAppMainView {
         }
     }
 }
+
+
 
 
